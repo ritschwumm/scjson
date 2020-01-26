661f8ceb0251f0c45db567041ddf0f39192c5a35
Herr Ritschwumm <ritschwumm@djini.de>
2020-01-26 16:05:03 +0100

    update dependencies, add converters for different ways to encode sum types

--- modules/converter/src/main/scala/converter/SumConverters.scala
+++ modules/converter/src/main/scala/converter/SumConverters.scala
@@ -16,4 +16,41 @@ object SumConverters {
 	val makeTagged:JsonConverter[(String,JsonValue),JsonValue]	=
 		(Converter total { kv:(String,JsonValue) => Seq(kv) })	>=>
 		JC.makeObject
+
+	//------------------------------------------------------------------------------
+
+	// NOTE this supports the format used in scjson-pickle's CaseClassProtocol.caseClassSumFormat
+
+	private val typeTag	= ""
+
+	// converts old { "": "type", ...foo } to new { "type": { ...foo }}
+	def extractTag:JsonConverter[JsonValue,JsonValue]	=
+		JC.expectObject >=>
+		Converter { parts =>
+			val map 	= parts.toMap
+			for {
+				tagVal	<-	map get typeTag	toGood JsonError("type tag not found")
+				tagStr	<-	tagVal.asString	toGood JsonError("type tag not a string")
+			}
+			yield {
+				val remainder	= map - typeTag
+				JsonObject.Var(tagStr -> JsonValue.mkObject(remainder.toVector))
+			}
+		}
+
+	// converts new { "type": { ...foo }} to old { "": "type", ...foo }
+	def injectTag:JsonConverter[JsonValue,JsonValue]	=
+		JC.expectObject >=>
+		Converter { it =>
+			for {
+				item	<-	it.singleOption	toGood JsonError(show"expected exactly one element, found ${it.size}")
+				(k, v)	=	item
+				payload	<-	v.asObject		toGood JsonError(show"expected payload to be an object")
+			}
+			yield {
+				JsonValue.mkObject(
+					payload	:+ (typeTag -> JsonValue.mkString(k))
+				)
+			}
+		}
 }
